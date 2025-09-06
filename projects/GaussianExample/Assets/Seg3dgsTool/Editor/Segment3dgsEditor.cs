bdf52ce3a0ce81d0f2a6f3d41f13dde2f5dcc4b6
김민겸(MinKyum Kim) <minemin7@g.skku.edu>
2025-09-06 16:46:03 +0900

    #7 | add SAMExtraction, scale tool

--- projects/GaussianExample/Assets/Seg3dgsTool/Editor/Segment3dgsEditor.cs
+++ projects/GaussianExample/Assets/Seg3dgsTool/Editor/Segment3dgsEditor.cs
@@ -6,23 +6,25 @@ using Seg3dgsTool.Runtime;
 
 namespace Seg3dgsTool.Editor
 {
-    [CustomEditor(typeof(GenerateVanilla3dgs))]
-    public class GenerateVanilla3dgsEditor : UnityEditor.Editor
+    [CustomEditor(typeof(Segment3dgs))]
+    public class Segment3dgsEditor : UnityEditor.Editor
     {
         SerializedProperty m_ColmapPath;
+        SerializedProperty m_Downsample;
 
         public void OnEnable()
         {
             m_ColmapPath = serializedObject.FindProperty("m_ColmapPath");
+            m_Downsample = serializedObject.FindProperty("m_Downsample");
         }
 
         public override void OnInspectorGUI()
         {
-            var tool = (GenerateVanilla3dgs)target;
+            var tool = (Segment3dgs)target;
 
             serializedObject.Update();
 
-            EditorGUILayout.LabelField("Generate Vanilla 3dgs", EditorStyles.boldLabel);
+           
 
             EditorGUI.BeginDisabledGroup(true);
             EditorGUILayout.PropertyField(m_ColmapPath, new GUIContent("COLMAP path"));
@@ -42,10 +44,35 @@ namespace Seg3dgsTool.Editor
                     }
                 }
                 
+                EditorGUILayout.Space(20);
+                
+
+                
+                EditorGUILayout.LabelField("Generate Vanilla 3dgs", EditorStyles.boldLabel);
+                EditorGUILayout.Space(2);
                 if (GUILayout.Button("Generate Vanilla 3dgs"))
                 {
                     tool.RunWslScriptViaServer();
                 }
+                
+                EditorGUILayout.Space(20);
+                EditorGUILayout.LabelField("Extract SAM Mask", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(m_Downsample, new GUIContent("Downsample Factor"));
+                EditorGUILayout.Space(2);
+                if (GUILayout.Button("Run SAM Mask Extraction"))
+                {
+                    tool.RunSamExtractionViaServer();
+                }
+                
+                
+                EditorGUILayout.Space(20);
+                EditorGUILayout.LabelField("Get corresponding mask scales", EditorStyles.boldLabel);
+                EditorGUILayout.Space(2);
+                if (GUILayout.Button("Run corresponding mask scales"))
+                {
+                    tool.RunGetScaleViaServer();
+                }
+
             }
 
             if (tool.IsRunning)
